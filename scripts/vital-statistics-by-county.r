3c2123e120f26099c0e1a529fab5060a6f2bedde
sinuous-sausage <edward.g.givens@gmail.com>
2016-07-30 21:33:39 -0500

    Add county files

---
+++ scripts/vital-statistics-by-county.r
@@ -0,0 +1,52 @@
+library(ggplot2)
+library(leaflet)
+library(plyr)
+library(rgdal)
+
+# Set your working directory to the folder containing the
+# county shapefile then read it into the R environment. 
+# It is available in the repo but can also be downloaded 
+# at https://www.census.gov/geo/maps-data
+
+counties <- readOGR(dsn = '.', layer = 'cb_2015_us_county_500k')
+
+# You've heard the rumors, but Texas does actually fit within 
+# the contiguous United States. Let's involve ourselves with 
+# its counties. Note that '48' identifies Texas' counties in 
+# the shapefile
+
+texas <- subset(counties, counties$STATEFP %in% '48')
+
+# Don't tell anyone that you created a handsome map of Texas
+# counties in three lines of code
+
+leaflet(texas) %>% 
+addProviderTiles('Stamen.TonerLite') %>% 
+addPolygons(color = '#002868', fillColor = '#BF0A30', 
+opacity = 1, fillOpacity = .75, popup = ~NAME)
+
+# What if you wanna spice it up with some demo data? Easy peasy!
+# First, download some data grouped by county. I again relied on
+# the Census Bureau for vital statistics. The metric of interest 
+# is on sheet five, column VST020207D (2007 live births per 1000)
+# http://www2.census.gov/prod2/statcomp/usac/excel/VST01.xls
+
+birthRate <- read_excel("C:/Users/e115174/Documents/GitHub/maps/counties/data/VST01.xls", 
+  sheet = 5, col_names = T)
+  
+texas@data$STCOU <- paste0(texas@data$STATEFP, texas@data$COUNTYFP)
+  
+texas <- merge(texas, birthRate, by = 'STCOU')
+
+leaflet(texas) %>% 
+addProviderTiles('Stamen.TonerLite') %>% 
+addPolygons(color = ~colorNumeric('RdYlGn', texas$VST020207D)(VST020207D), 
+stroke = F, fillOpacity = .9, popup = ~paste(NAME, VST020207D, sep = ' '), 
+group = 'Birth Rate') %>%
+addLegend('bottomright', pal = colorNumeric("RdYlGn", texas$VST020207D), values = ~VST020207D,
+    title = 'Birth Rate <br> (2007)', opacity = 1
+  ) %>% 
+addLayersControl(
+    overlayGroups = c('Birth Rate'),
+    options = layersControlOptions(collapsed = FALSE)
+  )
